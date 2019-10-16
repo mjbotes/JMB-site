385caadb7d98473466370ee5731880155baca003
Marnus BOTES <mbotes@c4r20s4.wethinkcode.co.za>
2019-10-16 09:42:12 +0200

    upload2

--- scripts/admin_add_p.php
+++ scripts/admin_add_p.php
@@ -1,10 +1,11 @@
 <?php
+	echo "<h2>Add Product</h2>";
 	echo '<form class="add_product" action="scripts/add_product.php" method="post" enctype="multipart/form-data">
 		<label>Product Name</label><input type="text" name="p_name"><br />
 		<label>Product Price</label><input type="int" name="price"><br />
 		<label>Product Description</label><input type="text"><br />
-		<input type="file" name="fileToUpload" id="fileToUpload">
-    	<input type="submit" value="Upload Image" name="submit">
+		<input type="file" name="fileToUpload" id="fileToUpload"><br />
+    	<input type="submit" value="ADD" name="submit">
 	</form>';
 ?>
 
--- scripts/modify.php
+++ scripts/modify.php
@@ -2,14 +2,14 @@
 	require_once "config.php";
 	if ($_POST["m_table"] === "ad_user")
 	{
-		$sql="UPDATE `users` SET `name`= $,`surname`= ? ,`email`= ?,`phone_no`= ? WHERE `user_id`= ?";
+		$sql="UPDATE `users` SET `name`=?,`surname`=? ,`email`=?,`phone_no`=? WHERE `user_id`=?";
 		if($stmt = mysqli_prepare($link, $sql)){
-			mysqli_stmt_bind_param($stmt, "ssss", $name, $s_name, $email, $p_no, $p_id);
+			mysqli_stmt_bind_param($stmt, "sssss", $name, $s_name, $email, $p_no, $p_id);
 			$name =$_POST["name"];
 			$s_name = $_POST["s_name"];
 			$email = $_POST["email"];
 			$p_no = $_POST["p_no"];
-			$p_id = $_POST["p-id"];
+			$p_id = $_POST["p_id"];
 			if(mysqli_stmt_execute($stmt)){
 				header("location: ../admin.php");
 			} else{
